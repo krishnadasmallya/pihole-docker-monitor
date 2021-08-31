a9d98eeb15aadfed622d37060f4a2739c55da78d
Krishnadas Mallya <krishnadasmallya@Krishnadass-MacBook-Air.local>
2021-08-31 21:41:06 +1000

    corrected the wrong paths install scripts

--- install.sh
+++ install.sh
@@ -3,19 +3,20 @@ set -e -x
 
 sudo apt update && sudo apt upgrade
 sudo apt install docker docker-compose  # portainer
+workdir=/opt/pihole
+sudo cp -r pihole/grafana /opt/
+sudo cp -r pihole/prometheus /opt/
+sudo cp -r node_exporter.service $workdir/
+sudo cp -r docker-compose.yml $workdir/
 
 cd /opt
-sudo mkdir -p {prometheus,pihole/{pihole,dnsmasq.d},grafana/provisioning/{datasources,dashboards},tmpinst}
+sudo mkdir -p {pihole/{pihole,dnsmasq.d}}
 IPAddr=`hostname -I | cut -d' ' -f1`
 #Download and modify configuration files
-sudo curl -s https://raw.githubusercontent.com/halod2003/pihole/main/prometheus.yml -o prometheus/prometheus.yml
-sudo curl -s https://raw.githubusercontent.com/halod2003/pihole/main/docker-compose.yml -o tmpinst/docker-compose.yml
-sudo curl -s https://raw.githubusercontent.com/halod2003/pihole/main/datasource.yml -o grafana/provisioning/datasources/datasource.yml
-sudo curl -sL https://raw.githubusercontent.com/halod2003/pihole/main/dashboard.yml  -o grafana/provisioning/dashboards/dashboard.yml
 sudo curl -sL https://grafana.com/api/dashboards/1860/revisions/23/download -o grafana/provisioning/dashboards/dash1.json
 sudo curl -sL https://grafana.com/api/dashboards/10176/revisions/2/download -o grafana/provisioning/dashboards/dash2.json
 sudo sed -i "s/IP_Addr/$IPAddr/" prometheus/prometheus.yml
-sudo sed -i "s/IP_Addr/$IPAddr/" tmpinst/docker-compose.yml
+sudo sed -i "s/IP_Addr/$IPAddr/" $workdir/docker-compose.yml
 sudo sed -i "s/IP_Addr/$IPAddr/" grafana/provisioning/datasources/datasource.yml
 
 ## 4) Install Node Exporter
@@ -23,17 +24,14 @@ sudo sed -i "s/IP_Addr/$IPAddr/" grafana/provisioning/datasources/datasource.yml
 sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-armv7.tar.gz
 sudo tar xfz node_exporter-1.1.2.linux-armv7.tar.gz
 sudo rm node_exporter-1.1.2.linux-armv7.tar.gz
-sudo mv node_exporter-1.1.2.linux-armv7/ node_exporter
-sudo curl -s https://raw.githubusercontent.com/halod2003/pihole/main/node_exporter.service -o /etc/systemd/system/node_exporter.service
+sudo mv node_exporter-1.1.2.linux-armv7 node_exporter
+sudo mv $workdir/node_exporter.service /etc/systemd/system/node_exporter.service
 sudo systemctl start node_exporter
 sudo systemctl enable node_exporter
 
 ## 5) Install services using docker-compose
 
-sudo docker-compose -f tmpinst/docker-compose.yml up -d
-
-#clean-up
-sudo rm -r tmpinst
+sudo docker-compose -f $workdir/docker-compose.yml up -d
 
 ## 6) Print access information
 
